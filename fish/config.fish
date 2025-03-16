990d9505d50b232b4704bd6d4fe1901eb60b28ae
sinder <smirnov.sinder@gmail.com>
2025-03-16 22:48:47 +0800

    Add dirty barely functioning fish config

---
+++ fish/config.fish
@@ -0,0 +1,136 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+end
+
+fish_vi_key_bindings
+function fish_user_key_bindings
+    # Dworak rebind
+    bind -s --preset -M default h backward-char
+    bind -s --preset -M default s forward-char
+    bind -s --preset -m insert a repaint-mode
+    bind -s --preset -m insert A beginning-of-line repaint-mode
+
+    bind -s --preset -m insert i 'set fish_cursor_end_mode exclusive' forward-single-char repaint-mode
+    bind -s --preset -m insert I 'set fish_cursor_end_mode exclusive' end-of-line repaint-mode
+
+    bind -s --preset -m visual V begin-selection repaint-mode
+
+    bind -s --preset n up-or-search
+    bind -s --preset t down-or-search
+
+    bind -s --preset d,i backward-jump-till and repeat-jump-reverse and begin-selection repeat-jump kill-selection end-selection
+    bind -s --preset d,a backward-jump and repeat-jump-reverse and begin-selection repeat-jump kill-selection end-selection
+end
+
+# TODO: this is the dvorak keymap
+# keymap.set("", "t", "j", remap_opts)
+# keymap.set("", "n", "k", remap_opts)
+# keymap.set("", "s", "l", remap_opts)
+# keymap.set("n", "a", "i", remap_opts)
+# keymap.set("n", "i", "a", remap_opts)
+# keymap.set("n", "I", "A", remap_opts)
+# keymap.set("n", "A", "I", remap_opts)
+
+fish_vi_key_bindings
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+# if test -f /home/sinder/anaconda3/bin/conda
+#     eval /home/sinder/anaconda3/bin/conda "shell.fish" hook $argv | source
+# else
+#     if test -f "/home/sinder/anaconda3/etc/fish/conf.d/conda.fish"
+#         . "/home/sinder/anaconda3/etc/fish/conf.d/conda.fish"
+#     else
+#         set -x PATH /home/sinder/anaconda3/bin $PATH
+#     end
+# end
+# <<< conda initialize <<<
+
+# TODO: path and aliases are kinda slow to source. optimize later.
+function ssource --description "source most of my dotfiles, useful if making changes and iterating"
+
+    source ~/.config/fish/path.fish
+    source ~/.config/fish/aliases.fish
+    source ~/.config/fish/functions.fish
+
+    # pull in all shared `export \u2026` aka `set -gx \u2026`
+    # source ~/.exports
+
+    # if test -e "../private/extras.fish"
+    #     source ../private/extras.fish
+    # end
+
+    # for things not checked into git..
+    # if test -e "$HOME/.extra.fish"
+    #     source ~/.extra.fish
+    # end
+end
+
+ssource
+
+# I don't need a prompt symbol for you-got-things-in-yr-stash
+set --erase pure_symbol_git_stash
+
+# Readline colors
+# set -g fish_color_autosuggestion 555 yellow
+# set -g fish_color_command 5f87d7
+# set -g fish_color_comment 808080
+# set -g fish_color_cwd 87af5f
+# set -g fish_color_cwd_root 5f0000
+# set -g fish_color_error 870000 --bold
+# set -g fish_color_escape af5f5f
+# set -g fish_color_history_current 87afd7
+# set -g fish_color_host 5f87af
+# set -g fish_color_match d7d7d7 --background=303030
+# set -g fish_color_normal normal
+# set -g fish_color_operator d7d7d7
+# set -g fish_color_param 5f87af
+# set -g fish_color_quote d7af5f
+# set -g fish_color_redirection normal
+# set -g fish_color_search_match --background=purple
+# set -g fish_color_status 5f0000
+# set -g fish_color_user 5f875f
+# set -g fish_color_valid_path --underline
+#
+# set -g fish_color_dimmed 555
+# set -g fish_color_separator 999
+
+# Git prompt status
+set -g __fish_git_prompt_showdirtystate yes
+set -g __fish_git_prompt_showupstream auto
+set -g pure_git_untracked_dirty false
+
+# pure
+set pure_threshold_command_duration 1
+set pure_separate_prompt_on_error true
+set pure_begin_prompt_with_current_directory false
+set -U pure_color_success (set_color green)
+set -U pure_color_git_dirty (set_color cyan)
+
+set -U pure_color_git_unpushed_commits (set_color yellow)
+set -U pure_color_git_unpulled_commits (set_color brgreen)
+
+# prompt (lucid)
+
+set -g lucid_prompt_symbol_error_color red
+
+# Status Chars
+#set __fish_git_prompt_char_dirtystate '*'
+set __fish_git_prompt_char_upstream_equal ''
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_dirtystate red
+
+set __fish_git_prompt_color_upstream_ahead ffb90f
+set __fish_git_prompt_color_upstream_behind blue
+
+# Local prompt customization
+set -e fish_greeting
+
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description 555 yellow
+set -g fish_pager_color_prefix cyan
+set -g fish_pager_color_progress cyan
---
+++ fish/functions.fish
@@ -0,0 +1,193 @@
+# thx to https://github.com/mduvall/config/
+
+function kill_process --description 'Kill process that user selects in fzf (from ps aux output)'
+    set -l pid (ps aux | fzf -m --header-lines=1 | awk '{print $2}')
+
+    if test -n "$pid"
+        echo "Killing processes: $pid"
+        kill -9 $pid
+    end
+end
+
+function kill_port --description 'Select a port to kill, by pid, port, or command line'
+
+    # Function to get the command line for a given PID
+    function get_command -a pid
+        ps -p $pid | awk 'NR>1 {for (i=4; i<=NF; i++) {printf "%s ", $i}; print ""}'
+    end
+
+    # Find listening processes, get commands, and format output
+    lsof -iTCP -sTCP:LISTEN -P | awk '{print $2, $9}' | uniq | tail -n +2 | while read -l pid port
+        set -l command (get_command $pid)
+        set -l port (string pad -w 8 (string replace 'localhost' '' $port))
+        set -l pid (string pad --right -w 6 $pid)
+        echo -e "$pid $port $command" | column
+    end |
+        # Pipe the output to fzf for selection. Grab pid and show pstree
+        fzf --exact --tac --preview 'pstree -p (echo {} | awk "{print $2}")' --preview-window=down,30% --header "Select a process to kill (PID Command Port):" |
+        # Kill the selected process
+        awk '{print $1}' | xargs -r kill -9
+end
+
+function clone --description "clone something, cd into it. install it."
+    git clone --depth=1 $argv[1]
+    cd (basename $argv[1] | sed 's/.git$//')
+    yarn install
+end
+
+function renameurldecode
+    for file in *
+        set -l original $file
+        set -l decoded (python3 -c "import urllib.parse; print(urllib.parse.unquote_plus('$original'))")
+        if test "$original" != "$decoded"
+            mv "$original" "$decoded"
+        end
+    end
+end
+
+function beep --description "make two beeps"
+    echo -e '\a'
+    sleep 0.1
+    echo -e '\a'
+
+end
+
+function all_binaries_in_path --description \
+    "list all binaries available in \$PATH (incl conflicts). pipe it to grep. top-most are what's used, in case of conflicts"
+    # based on https://unix.stackexchange.com/a/120790/110766 but tweaked to work on mac. and then made it faster.
+    find -L $PATH -maxdepth 1 -executable -type f 2>/dev/null
+
+    # list path
+    # for val in $PATH; echo "$val"; end
+end
+
+function list_path --description "list all paths in PATH"
+    for val in $PATH
+        echo "$val"
+    end
+end
+
+function my_paths --description "list paths, in order"
+    echo "#  "
+    printf '%s\n' (string split \n $PATH)
+end
+
+function stab --description "stabalize a video"
+    set -l vid $argv[1]
+    ffmpeg -i "$vid" -vf vidstabdetect=stepsize=32:result="$vid.trf" -f null -
+
+    ffmpeg -i "$vid" -b:v 5700K -vf vidstabtransform=interpol=bicubic:input="$vid.trf" "$vid.mkv"
+    # :optzoom=2 seems nice in theory but i dont love it. kinda want a combo of 1 and 2. (dont zoom in past the static zoom level, but adaptively zoom out to full when possible)
+    ffmpeg -i "$vid" -i "$vid.mkv" -b:v 3000K -filter_complex hstack "$vid.stack.mkv"
+    # vid=Dalton1990/Paultakingusaroundthehouseagai ffmpeg -i "$vid.mp4" -i "$vid.mkv" -b:v 3000K -filter_complex hstack $HOME/Movies/"Paultakingusaroundthehouseagai.stack.mkv"
+    command rm $vid.trf
+end
+
+function md --wraps mkdir -d "Create a directory and cd into it"
+    command mkdir -p $argv
+    if test $status = 0
+        switch $argv[(count $argv)]
+            case '-*'
+            case '*'
+                cd $argv[(count $argv)]
+                return
+        end
+    end
+end
+
+# print compression results with a bar chart
+function gz --d "Get the gzipped size"
+    set -l file "$argv[1]"
+    set -l orig_size (cat "$file" | wc -c)
+
+    printf "\e[1;97m%-20s %12s\e[0m\n" "compression method" bytes
+
+    for method in \
+        original \
+        # Gzip CLI default is -6, but GH pages only uses -5. Dunno about others.
+        "gzip (-5)" \
+        # "gzip (--best)" \
+        # (test (command -v zstd) && echo "zstd (-3)") \
+        (test (command -v zstd) && echo "zstd") \
+        (test (command -v brotli) && echo "brotli (-q 5)") # brotli is last because its compressor is sloowww
+
+        printf "%-20s " "$method"
+
+        set -l compressed_size (
+      switch "$method"
+        case "original"
+          cat "$file" | wc -c
+        case "gzip (-5)"
+          cat "$file" | gzip -5 -c | wc -c
+        case "gzip (--best)"
+          cat "$file" | gzip --best -c | wc -c
+        case "brotli (-q 5)"
+          cat "$file" | brotli -c  | wc -c
+        case "zstd"
+          # If experimenting, could also do --19 or --22 --ultra
+          cat "$file" | zstd -c - | wc -c
+        case '*'
+          echo "Unhandled case. $method"
+      end
+    )
+        set -l wid (math $COLUMNS - 40)
+        set -l bar_width (math -s0 $compressed_size \* $wid / $orig_size)
+        printf "%'12.0f   %s%s\n" "$compressed_size" (string repeat -n $bar_width '█') (string repeat -n (math -s0 "$wid - $bar_width")  '░')
+    end
+end
+
+function sudo!!
+    eval sudo $history[1]
+end
+
+# `shellswitch [bash|zsh|fish]`
+function shellswitch
+    chsh -s (brew --prefix)/bin/$argv
+end
+
+# requires my excellent `npm install -g statikk`
+# function server -d 'Start a HTTP server in the current dir, optionally specifying the port'
+#     # arg can either be port number or extra args to statikk
+#     if test $argv[1]
+#         if string match -qr '^-?[0-9]+(\.?[0-9]*)?$' -- "$argv[1]"
+#             set port $argv[1]
+#             # fancy argv thing to pass all remaining args. eg `server --cors --jsprof`
+#             statikk --open --port $argv[1..-1]
+#         else
+#             statikk --open $argv[1..-1]
+#         end
+#
+#     else
+#         statikk --open
+#     end
+# end
+
+function conda -d 'lazy initialize conda'
+    functions --erase conda
+    if test -f /home/sinder/anaconda3/bin/conda
+        eval /home/sinder/anaconda3/bin/conda "shell.fish" hook $argv | source
+    else
+        if test -f "/home/sinder/anaconda3/etc/fish/conf.d/conda.fish"
+            . "/home/sinder/anaconda3/etc/fish/conf.d/conda.fish"
+        else
+            set -x PATH /home/sinder/anaconda3/bin $PATH
+        end
+    end
+    # There's some opportunity to use `psub` but I don't really understand it.
+    conda $argv
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+# <<< conda initialize <<<
+
+# NVM doesnt support fish and its stupid to try to make it work there.
+
+function google_cloud_sdk_lazy_init -d 'Lazy initializer for Google Cloud SDK'
+    functions --erase gcloud gsutil bq
+    source "$HOME/google-cloud-sdk/path.fish.inc"
+    $argv
+end
+for cmd in gcloud gsutil bq
+    eval "function $cmd; google_cloud_sdk_lazy_init $cmd \$argv; end"
+end
