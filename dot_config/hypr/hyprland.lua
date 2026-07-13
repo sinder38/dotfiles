88b2e0ce51f2ffefba0e5a886d869c2095b8b551
sinder <smirnov.sinder@gmail.com>
2026-07-13 20:15:18 +0800

    mirror configuration for connected displays todos zed migration back to alt for panel navigation and ctl for menu navigation fix zelij fish completion keybind conflict zed git group by none starship prompt

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -11,11 +11,20 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-    output   = "",
+    output   = "eDP-1",
     mode     = "preferred",
     position = "auto",
     scale    = 1.5,
 })
+
+hl.monitor({
+    output   = "",
+    mode     = "preferred",
+    position = "auto",
+    scale    = 1,
+    mirror = "eDP-1"
+})
+
 
 
 ---------------------
